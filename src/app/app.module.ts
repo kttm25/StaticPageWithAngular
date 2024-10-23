26036ca71170c4a37d41cb1943d9a4f13d0bba86
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-10-22 21:40:12 -0400

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { ContainerComponent } from './components/container/container.component';
+import { ArticleComponent } from './components/article/article.component';
+import { NavComponent } from './components/nav/nav.component';
+import { Container2Component } from './components/container2/container2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    AsideComponent,
+    ContainerComponent,
+    ArticleComponent,
+    NavComponent,
+    Container2Component
   ],
   imports: [
     BrowserModule,
